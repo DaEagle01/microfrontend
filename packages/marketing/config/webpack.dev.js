c40343c8bb26f93d9843020b217716bc957d0f1d
Fahad Bin Karim <fbinkarim01@gmail.com>
2025-10-04 17:16:32 +0600

    Refactor webpack configuration: consolidate HtmlWebpackPlugin usage and clean up imports

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,8 +1,7 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
-const packageJson = require('../package.json');
+const packageJson = require("../package.json");
 
 const devConfig = {
   mode: "development",
@@ -19,10 +18,7 @@ const devConfig = {
       exposes: {
         "./MarketingApp": "./src/bootstrap",
       },
-      shared: packageJson.dependencies
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      shared: packageJson.dependencies,
     }),
   ],
 };
