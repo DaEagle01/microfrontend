9ec28021f19dc4b816a219057fca7e10919b59ea
Fahad Bin Karim <fbinkarim01@gmail.com>
2025-10-06 23:53:14 +0600

    update container app production webpack config

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,6 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
+const containerDomain = process.env.CONTAINER_PRODUCTION_DOMAIN;
 const marketingDomain = process.env.MARKETING_PRODUCTION_DOMAIN;
 const authDomain = process.env.AUTH_PRODUCTION_DOMAIN;
 
@@ -11,7 +12,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: 'auto',
+    publicPath: containerDomain,
   },
   plugins: [
     new ModuleFederationPlugin({
@@ -23,7 +24,7 @@ const prodConfig = {
       shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: "./public/index.html",
     }),
   ],
 };
